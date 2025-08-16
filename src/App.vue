75d9a7e6ef82120e6328e0cbda9bbb80f2cdfb95
rizky.ariananda <rizkyari4121@gmail.com>
2025-08-16 16:09:28 +0700

    feat: sets the job list and its components

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import Footer from './components/layout/Footer.vue';
 </script>
 
 <template>
-  <div class="min-h-screen flex flex-col bg-gray-50 text-gray-900">
+  <div class="min-h-screen flex flex-col bg-brand-bg text-gray-900">
     <Header />
 
     <main class="mx-auto max-w-6xl px-4 py-6">
